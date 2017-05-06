d0d7f3cf39b57e42eda757f873fbf1793f496036
Michael Michailidis <blitzkriegassault@gmail.com>
2017-05-06 21:40:15 +0300

    navBar fixed

--- app/Http/Controllers/Helpers.php
+++ app/Http/Controllers/Helpers.php
@@ -3,27 +3,52 @@
 
 namespace App\Http\Controllers;
 
+use App\Admin;
+use App\Company;
+use App\Employee;
+use App\Member;
+use Illuminate\Support\Facades\Auth;
 
 class Helpers
 {
     public static function isMember()
     {
-        return true;
+        $member = Member::where(self::getQuery())->first();
+
+        return $member != null;
     }
 
     public static function isAdmin()
     {
-        return true;
+        $admin = Admin::where(self::getQuery())->first();
+
+        return $admin != null;
     }
 
     public static function isEmployee()
     {
-        return true;
+        $employee = Employee::where(self::getQuery())->first();
+
+        return $employee != null;
     }
 
     public static function isVerificationRequired()
     {
-        return true;
+        $admin = Admin::where(self::getQuery())->first();
+
+        if($admin == null) {
+            return false;
+        }
+
+        $company = Company::find($admin->CompanyId);
+
+        return $company == null ? false : $company->VerificationRequired;
+    }
+
+    private static function getQuery()
+    {
+        return [
+            "UserId" =>  Auth::getUser()->id
+        ];
     }
 }
-
