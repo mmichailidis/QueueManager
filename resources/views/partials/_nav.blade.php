50ee41644aff79a85b3c633a0720d2f33131e341
Konstantina_Avg <konavge93@gmail.com>
2017-05-06 18:54:18 +0300

    button on nav

--- resources/views/partials/_nav.blade.php
+++ resources/views/partials/_nav.blade.php
@@ -30,7 +30,7 @@
                     <li><a href="{{ url('/contact') }}">Επικοινωνία</a></li>
                 @if (Auth::guest())
                     <li><a href="{{ url('/login') }}">Login</a></li>
-                    <li><a href="{{ url('/register') }}">Register</a></li>
+                    <li><a href="{{ url('/register') }}"><p class="navbar-btn"><button type="button" class="btn btn-success">Εγγραφή <span class="glyphicon glyphicon-log-in"></span></button> &nbsp;</p></a></li>
                 @else
                     <li class="dropdown">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
