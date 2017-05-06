97c3cfaccc470fdfb840afb69d383cdd036a6f95
Michael Michailidis <blitzkriegassault@gmail.com>
2017-05-06 19:20:21 +0300

    AdminMemberController - index done

--- app/Http/Controllers/Admin/AdminMemberController.php
+++ app/Http/Controllers/Admin/AdminMemberController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Verification;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
@@ -9,27 +10,45 @@ use App\Http\Controllers\Controller;
 
 class AdminMemberController extends Controller
 {
-    public function index() {
+    public function index()
+    {
         $company = AdminHelper::myCompany();
 
-        if(!$company->VerificationRequired) {
+        if (!$company->VerificationRequired) {
             return redirect()->route('admin.show');
         }
+
+        $query = ['CompanyId' => $company->Id];
+        $verification = Verification::where($query)->get();
+
+        $membersData = [];
+
+        foreach ($verification as $single) {
+            array_push($membersData, AdminHelper::getMemberInfo($single->MemberId));
+        }
+
+        dd($membersData);
+
+        return view('admin.member.index')->with('members', $membersData);
     }
 
-    public function create() {
+    public function create()
+    {
 
     }
 
-    public function store() {
+    public function store()
+    {
 
     }
 
-    public function show($id) {
+    public function show($id)
+    {
 
     }
 
-    public function destroy($id) {
+    public function destroy($id)
+    {
 
     }
 }
