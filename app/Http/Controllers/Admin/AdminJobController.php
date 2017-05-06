35808d4b9abb96d2355be6d15849579cb3407735
Michael Michailidis <blitzkriegassault@gmail.com>
2017-05-06 17:29:50 +0300

    adminJobController is done

--- app/Http/Controllers/Admin/AdminJobController.php
+++ app/Http/Controllers/Admin/AdminJobController.php
@@ -2,38 +2,88 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Job;
 use Illuminate\Http\Request;
 
-use App\Http\Requests;
 use App\Http\Controllers\Controller;
 
 class AdminJobController extends Controller
 {
-    public function index() {
-        return view('admin.job.index')->with('jobs',AdminHelper::allJobs());
+    public function index()
+    {
+        return view('admin.job.index')->with('jobs', AdminHelper::allJobs());
     }
 
-    public function create() {
+    public function create()
+    {
+        return view('admin.job.create')->with('jobs', AdminHelper::allJobs());
+    }
+
+    public function store(Request $request)
+    {
+        $job = Job::create([
+            'CompanyId' => AdminHelper::getAdmin()->CompanyId,
+            'Name' => $request->input('Name'),
+            'IsByName' => $request->input('IsByName'),
+            'TypeOfJob' => $request->input('TypeOfJob'),
+            'AverageWaitingTime' => 0,
+        ]);
+
+        return redirect()->route('admin.job.show', $job->Id);
+    }
+
+    public function show($id)
+    {
+        $job = Job::find($id);
+
+        if ($job->CompanyId != AdminHelper::getAdmin()->CompanyId) {
+            return redirect()->route('admin.job.index');
+        }
+
+        return view('admin.job.show')->with('job', $job);
+    }
+
+    public function edit($id)
+    {
+        $job = Job::find($id);
+
+        if ($job->CompanyId != AdminHelper::getAdmin()->CompanyId) {
+            return redirect()->route('admin.job.index');
+        }
+
+        return view('admin.job.show')->with('job', $job);
 
     }
 
-    public function store() {
+    public function update(Request $request, $id)
+    {
+        $job = Job::find($id);
 
+        if ($job->CompanyId != AdminHelper::getAdmin()->CompanyId) {
+            return redirect()->route('admin.job.index');
+        }
+
+        $avgTime = $job->AverageWaitingTime;
+
+        $job->update([
+            'Name' => $request->input('Name'),
+            'IsByName' => $request->input('IsByName'),
+            'LastNumber' => 0,
+            'TypeOfJob' => $request->input('TypeOfJob'),
+            'AverageWaitingTime' => $avgTime,
+        ]);
+
+        return redirect()->route('admin.job.show', $job->Id);
     }
 
-    public function show($id) {
+    public function destroy($id)
+    {
+        $job = Job::find($id);
 
-    }
+        if ($job->CompanyId != AdminHelper::getAdmin()->CompanyId) {
+            return redirect()->route('admin.job.index');
+        }
 
-    public function edit($id) {
-
-    }
-
-    public function update(Request $request) {
-
-    }
-
-    public function destroy($id) {
-
+        $job->delete();
     }
 }
