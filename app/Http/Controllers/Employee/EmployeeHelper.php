80c58f7f9c3ebb8f8ee32f6301656268d8ca4505
OrestisZ <orestispaok@windowslive.com>
2017-05-06 20:19:18 +0300

    Employee Controller myProfile and myJob created. Functionality added on EmployeeHelper

--- app/Http/Controllers/Employee/EmployeeHelper.php
+++ app/Http/Controllers/Employee/EmployeeHelper.php
@@ -8,8 +8,32 @@
 
 namespace App\Http\Controllers\Employee;
 
+use App\Employee;
+use App\Job;
+use App\User;
+use Illuminate\Support\Facades\Auth;
+
 
 class EmployeeHelper
 {
+    static function getEmployee() {
+        $query = ['UserId' => Auth::getUser()->id];
 
+        return Employee::where($query)->first();
+    }
+
+    static function getJob() {
+        $query = ['CompanyId' => self::getEmployee()->JobId];
+
+        return Job::where($query)->get();
+    }
+
+    static function getEmployeeInfo() {
+        $user = User::find(self::getEmployee()->UserId);
+
+        return [
+            'name' => $user->name,
+            'email' => $user->email,
+        ];
+    }
 }
