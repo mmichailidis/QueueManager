b12521615d6d6c4a7b3c04a2f7ff99b4e47cff0d
OrestisZ <orestispaok@windowslive.com>
2017-05-06 19:16:36 +0300

    AdminMemberController show and destroy funcs done

--- app/Http/Controllers/Admin/AdminMemberController.php
+++ app/Http/Controllers/Admin/AdminMemberController.php
@@ -26,10 +26,25 @@ class AdminMemberController extends Controller
     }
 
     public function show($id) {
+        $company = AdminHelper::myCompany();
 
+        if(!$company->VerificationRequired) {
+            return redirect()->route('admin.show');
+        }
+
+        return view('admin.member.show')->with('member', AdminHelper::getMemberInfo($id));
     }
 
     public function destroy($id) {
+        $company = AdminHelper::myCompany();
 
+        if(!$company->VerificationRequired) {
+            return redirect()->route('admin.show');
+        }
+
+        $member = AdminHelper::getMemberInfo($id);
+        $member->delete();
+
+        return redirect()->route('admin.member.index');
     }
 }
