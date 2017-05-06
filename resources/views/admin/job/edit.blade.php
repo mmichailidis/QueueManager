e7e14c926360ea4ed2ba1eba040a74ba28d12f43
iwannafwt <iwanna.fwtiadoy@gmail.com>
2017-05-07 01:02:09 +0300

    Admin/jobs/edit Data

--- resources/views/admin/job/edit.blade.php
+++ resources/views/admin/job/edit.blade.php
@@ -1,1 +1,20 @@
-<?php
+{!! Form::model($job, ['route' => ['admin.job.update', $job->Id], 'method' => 'PUT']) !!}
+
+{{ Form::label('Name', 'Name:') }}
+{{ Form::text('Name', null, ["class" => 'form-control input-lg']) }}
+
+{{ Form::label('IsByName', 'IsByName:') }}
+{{ Form::text('IsByName', null, ["class" => 'form-control input-lg']) }}
+
+{{ Form::label('LastNumber', 'LastNumber:') }}
+{{ Form::text('LastNumber', null, ["class" => 'form-control input-lg']) }}
+
+{{ Form::label('TypeOfJob', 'TypeOfJob:') }}
+{{ Form::text('TypeOfJob', null, ["class" => 'form-control input-lg']) }}
+
+{{ Form::label('AverageWaitingTime', 'AverageWaitingTime:') }}
+{{ Form::text('AverageWaitingTime', null, ["class" => 'form-control input-lg']) }}
+
+{{ Form::submit('Save Changes', ['class' => 'btn btn-success btn-block']) }}
+
+{!! Form::close() !!}
