1bf308c71a0b1293e4208136a95b61716abebed0
OrestisZ <orestispaok@windowslive.com>
2017-05-06 12:30:34 +0300

    minor refactor in db + some models ready

--- app/Category.php
+++ app/Category.php
@@ -4,7 +4,25 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property mixed Id
+ * @property mixed Name
+ * @property mixed Photo
+ * @property mixed TypeOfCategory
+ * @property mixed created_at
+ * @property mixed updated_at
+ */
 class Category extends Model
 {
-    //
+    protected $primaryKey = 'Id';
+
+    protected $table = 'categories';
+
+    protected $fillable = [
+        'Name',
+        'Photo',
+        'TypeOfCategory'
+    ];
+
+    protected $guarded = 'Id';
 }
--- app/Company.php
+++ app/Company.php
@@ -4,7 +4,30 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property mixed Id
+ * @property mixed CategoryId
+ * @property mixed AutoProceedActivated
+ * @property mixed AutoProceedTime
+ * @property mixed VerificationRequired
+ * @property mixed ImageLocation
+ * @property mixed created_at
+ * @property mixed updated_at
+ */
+
 class Company extends Model
 {
-    //
+    protected $primaryKey = 'Id';
+
+    protected $table = 'companies';
+
+    protected $fillable = [
+        'CategoryId',
+        'AutoProceedActivated',
+        'AutoProceedTime',
+        'VerificationRequired',
+        'ImageLocation'
+    ];
+
+    protected $guarded = 'Id';
 }
