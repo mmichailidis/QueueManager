b1e4f96831c8dcc3d7f0b9c3158c41a82e01882d
OrestisZ <orestispaok@windowslive.com>
2017-05-06 12:47:55 +0300

    Models are ready

--- app/Job.php
+++ app/Job.php
@@ -4,7 +4,32 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property mixed Id
+ * @property mixed CompanyId
+ * @property mixed Name
+ * @property mixed IsByName
+ * @property mixed LastNumber
+ * @property mixed TypeOfJob
+ * @property mixed AverageWaitingTime
+ * @property mixed created_at
+ * @property mixed updated_at
+ */
+
 class Job extends Model
 {
-    //
+    protected $primaryKey = 'Id';
+
+    protected $table = 'jobs';
+
+    protected $fillable = [
+        'CompanyId',
+        'Name',
+        'IsByName',
+        'LastNumber',
+        'TypeOfJob',
+        'AverageWaitingTime'
+    ];
+
+    protected $guarded = 'Id';
 }
--- app/Thread.php
+++ app/Thread.php
@@ -4,7 +4,30 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property mixed Id
+ * @property mixed MemberId
+ * @property mixed EmployeeId
+ * @property mixed MessageCount
+ * @property mixed IsActive
+ * @property mixed RequestForEnd
+ * @property mixed created_at
+ * @property mixed updated_at
+ */
+
 class Thread extends Model
 {
-    //
+    protected $primaryKey = 'Id';
+
+    protected $table = 'threads';
+
+    protected $fillable = [
+        'MemberId',
+        'EmployeeId',
+        'MessageCount',
+        'IsActive',
+        'RequestForEnd'
+    ];
+
+    protected $guarded = 'Id';
 }
