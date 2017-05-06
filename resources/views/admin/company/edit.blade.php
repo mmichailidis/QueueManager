a8a9776ec99a7f33cdc668c6f87698ab101f19bc
Konstantina_Avg <konavge93@gmail.com>
2017-05-07 01:42:12 +0300

    admin edit company

--- resources/views/admin/company/edit.blade.php
+++ resources/views/admin/company/edit.blade.php
@@ -1,17 +1,35 @@
-{!! Form::model($company, ['route' => ['admin.update', $company->Id], 'method' => 'PUT']) !!}
+@extends('layouts.app')
+@section('title' , '| Επεξεργασία στοιχείων')
+@section('content')
 
-{{ Form::label('Name', 'Name:') }}
-{{ Form::text('Name', null, ["class" => 'form-control input-lg']) }}
+</br></br>
+<h2 style="margin-left:70px;">Επεξεργασία στοιχείων</h2>
+<div class="container text-center">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2 center">
+        <div class="jumbotron">
+            <div class="col-md-6 col-xs-push-3 center" id="formTemplJump">
 
-{{ Form::label('AutoProceedActivated', 'AutoProceedActivated:') }}
-{{ Form::text('AutoProceedActivated', null, ["class" => 'form-control input-lg']) }}
+            {!! Form::model($company, ['route' => ['admin.update', $company->Id], 'method' => 'PUT']) !!}
 
-{{ Form::label('AutoProceedTime', 'AutoProceedTime:') }}
-{{ Form::text('AutoProceedTime', null, ["class" => 'form-control input-lg']) }}
+            {{ Form::label('Name', 'Όνομα:') }}
+            {{ Form::text('Name', null, ["class" => 'form-control input-lg']) }}
+            <br>
+            {{ Form::label('AutoProceedActivated', 'Αυτόματη κίνηση γραμμής:') }}
+            {{ Form::text('AutoProceedActivated', null, ["class" => 'form-control input-lg']) }}
+            <br>
+            {{ Form::label('AutoProceedTime', 'Αυτόματη συνέχεια χρόνου:') }}
+            {{ Form::text('AutoProceedTime', null, ["class" => 'form-control input-lg']) }}
+            <br>
+            {{ Form::label('VerificationRequired', 'Απαιτείται επαλήθευση:') }}
+            {{ Form::text('VerificationRequired', null, ["class" => 'form-control input-lg']) }}
+            <br>
+            {{ Form::submit('Αποθήκευση', ['class' => 'btn btn-primary btn-block']) }}
 
-{{ Form::label('VerificationRequired', 'VerificationRequired:') }}
-{{ Form::text('VerificationRequired', null, ["class" => 'form-control input-lg']) }}
-
-{{ Form::submit('Save Changes', ['class' => 'btn btn-success btn-block']) }}
-
-{!! Form::close() !!}
+            {!! Form::close() !!}
+            </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
