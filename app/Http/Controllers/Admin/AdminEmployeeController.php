ae469fefa3b075d64cd5aeb33395fb37cec7ae3d
OrestisZ <orestispaok@windowslive.com>
2017-05-06 18:02:40 +0300

    employee controller (not finished)

--- app/Http/Controllers/Admin/AdminEmployeeController.php
+++ app/Http/Controllers/Admin/AdminEmployeeController.php
@@ -12,32 +12,50 @@ class AdminEmployeeController extends Controller
 {
     public function index() {
         $employees = Employee::all();
-        dd($employees);
-        dd(AdminHelper::getMemberInfo($employees->Id));
-        return view('admin.employee.index')->with('employees', $employees)->with('data',AdminHelper::getMemberInfo($employees->Id));
+        return view('admin.employee.index')->with('employees', $employees)->with('data',AdminHelper::getEmployeeInfo($employees->Id));
     }
 
     public function create() {
 
     }
 
-    public function store() {
+    public function store(Request $request) {
+        $employee = Employee::create([
+            'IsOnline' => $request->input('IsOnline'),
+            'JobId' => $request->input('JobId'),
+            'UserId' => $request->input('UserId')
+        ]);
 
+        return redirect()->route('admin.employee.show')->with('employee' , $employee->Id);
     }
 
     public function show($id) {
-
+        $employee = Employee::find($id);
+        dd(AdminHelper::getMemberInfo($employee->Id));
+        return view('admin.employee.show')->with('employee', $employee)->with('data', AdminHelper::getEmployeeInfo($employee->Id));
     }
 
     public function edit($id) {
-
+        $employee = Employee::find($id);
+        return view('admin.employee.edit')->with('employee', $employee);
     }
 
-    public function update(Request $request) {
+    public function update(Request $request, $id) {
+        $employee = Employee::find($id);
 
+        $employee->update([
+            'IsOnline' => $request->input('IsOnline'),
+            'JobId' => $request->input('JobId'),
+            'UserId' => $request->input('UserId')
+        ]);
+
+        return redirect()->route('admin.employee.show')->with('employee' , $employee->Id);
     }
 
     public function destroy($id) {
+        $employee = Employee::find($id);
+        $employee->delete();
 
+        return redirect()->route('admin.employee.index');
     }
 }
