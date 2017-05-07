ac9da1f62ffedd09950f775d55a5393a53dfefb6
iwannafwt <iwanna.fwtiadoy@gmail.com>
2017-05-07 13:57:30 +0300

    errorBlade

--- resources/views/member/ticket/error.blade.php
+++ resources/views/member/ticket/error.blade.php
@@ -1,9 +1,21 @@
-
 @extends('layouts.app')
 @section('title' , '| Eργαζόμενοι')
 @section('content')
-<br><br>
+    <br><br>
+    <div><br><br></div>
 
-<p>Το νούμερο που ζητάτε δεν υπάρχει ή δεν έχετε άδεια να βγάλετε νούμερο!</p>
+    <div class="row">
+        <div class="col-md-6"></div>
+        <div class="col-md-6" style="margin-left: 40%">
+            <img src="{{ asset("images/sorry.gif") }}" class="img-responsive" alt="Emoji"/>
+            <div class="card-info"><h2 class="card-title">Συγνώμη . . .</h2></div>
+            <br><br>
+
+        </div>
+    </div>
+    <div class="row">
+        <h2>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Το νούμερο που ζητάτε δεν υπάρχει ή δεν έχετε άδεια να βγάλετε νούμερο!</h2>
+    </div>
+
 @endsection
 
