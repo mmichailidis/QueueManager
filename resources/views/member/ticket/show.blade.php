c910a29da4fa4319a8197aca1afc41178aeb7b04
iwannafwt <iwanna.fwtiadoy@gmail.com>
2017-05-07 09:41:25 +0300

    ticket show done

--- resources/views/member/ticket/show.blade.php
+++ resources/views/member/ticket/show.blade.php
@@ -1,1 +1,45 @@
-<?php
+@extends('layouts.app')
+@section('title' , '| Eργαζόμενοι')
+@section('content')
+    <div class="container">
+        <div class="row clearfix">
+            <div class="col-md-12 table-responsive">
+                <table class="table table-bordered table-hover table-sortable" id="tab_logic">
+                    <thead>
+                    <tr>
+                        <th class="text-center">
+                            Νούμερο
+                        </th>
+                        <th class="text-center">
+                            Ώρα
+                        </th>
+                        <th class="text-center">
+                            Μέσος χρόνος αναμονής
+                        </th>
+                        <th class="text-center">
+                            Αριθμός που εξυπηρετήτε τώρα
+                        </th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td data-name="Number">
+                            {{$ticket->Number}}
+                        </td>
+                        <td data-name="Time">
+                            {{\App\Service\TimeConverter::fromUnix($ticket->Time)}}
+                        </td>
+                        <td data-name="Go">
+                            {{$jobAverageTime}}
+                        </td>
+                        <td data-name="Del">
+                            {{$jobCurrentNumber}}
+                        </td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+@endsection
+
