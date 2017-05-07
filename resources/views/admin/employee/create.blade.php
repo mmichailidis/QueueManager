74bbb847af303075e475ced0c4ff30c580f2e749
Konstantina_Avg <konavge93@gmail.com>
2017-05-07 03:53:16 +0300

    br fixed

--- resources/views/admin/employee/create.blade.php
+++ resources/views/admin/employee/create.blade.php
@@ -4,36 +4,36 @@
 
 @section('content')
     {{--{{ dd($employees, $jobs) }}--}}
-</br></br>
-<h2 style="margin-left:70px;">Δημιουργία νέου εργαζόμενου</h2>
+    <br><br>
+    <h2 style="margin-left:70px;">Δημιουργία νέου εργαζόμενου</h2>
     <div class="container text-center">
         <div class="row">
             <div class="col-md-8 col-md-offset-2 center">
                 <div class="jumbotron">
                     <div class="col-md-6 col-xs-push-3 center" id="formTemplJump">
-                {!! Form::open(['route' => 'admin.employee.store', 'class' => 'form']) !!}
-                    </br>
-                    {{ Form::label('name', 'Όνομα: ') }}
-                    {{ Form::text('name', null, ['class' => 'form-control input-sm']) }}
-                    </br>
-                    {{ Form::label('email', 'Email: ') }}
-                    {{ Form::text('email', null, ['class' => 'form-control input-sm']) }}
-                    </br>
-                    {{ Form::label('password', 'Κωδικός πρόσβασης: ') }}
-                    {{ Form::password('password', ['class' => 'form-control input-sm']) }}
-                    </br>
-                    {{ Form::label('JobId', 'Θέση εργασίας: ') }}
-                    <select name="JobId" title="JobId" class=" form-control input-sm">
-                        @foreach($jobs as $job)
-                            <option value="{{ $job->Id }}">{{ $job->Name }}</option>
-                        @endforeach
-                    </select>
-                        </br> </br>
-                    {{ Form::submit('Δημιουργία', ['class' => 'btn btn-block btn-success']) }}
-                {!! Form::close() !!}
+                        {!! Form::open(['route' => 'admin.employee.store', 'class' => 'form']) !!}
+                        <br>
+                        {{ Form::label('name', 'Όνομα: ') }}
+                        {{ Form::text('name', null, ['class' => 'form-control input-sm']) }}
+                        <br>
+                        {{ Form::label('email', 'Email: ') }}
+                        {{ Form::text('email', null, ['class' => 'form-control input-sm']) }}
+                        <br>
+                        {{ Form::label('password', 'Κωδικός πρόσβασης: ') }}
+                        {{ Form::password('password', ['class' => 'form-control input-sm']) }}
+                        <br>
+                        {{ Form::label('JobId', 'Θέση εργασίας: ') }}
+                        <select name="JobId" title="JobId" class=" form-control input-sm">
+                            @foreach($jobs as $job)
+                                <option value="{{ $job->Id }}">{{ $job->Name }}</option>
+                            @endforeach
+                        </select>
+                        <br> <br>
+                        {{ Form::submit('Δημιουργία', ['class' => 'btn btn-block btn-success']) }}
+                        {!! Form::close() !!}
+                    </div>
+                </div>
             </div>
-        </div>
-    </div>
         </div>
     </div>
 @endsection
